0024616645f17acdf43cbc76534e19c5483ac937
José Ramón Méndez Reboredo <moncho.mendez@gmail.com>
2019-10-07 15:08:19 +0200

    Small bugfix

--- src/main/java/org/datasetservice/Main.java
+++ src/main/java/org/datasetservice/Main.java
@@ -12,17 +12,12 @@ import org.datasetservice.domain.TaskCreateUPreprocessing;
 import org.datasetservice.domain.TaskCreateUdataset;
 import org.datasetservice.preprocessor.Preprocessor;
 
-import org.apache.commons.dbcp2.BasicDataSource;
 
 public class Main {
 
     static final long CHECK_INTERVAL_MS = 5000;
 
     public static void main(String[] args) throws Exception {
-        BasicDataSource ds = new BasicDataSource();
-        ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        ds.setUsername(getProperty("user"));
-        ds.setPassword(getProperty("password"));
 
         String datasetStorage = getProperty("datasetStorage");
         String pipelineStorage = getProperty("pipelineStorage");
@@ -38,7 +33,8 @@ public class Main {
 
         Long startTime = System.currentTimeMillis();
         while (true) {
-            Thread.sleep(CHECK_INTERVAL_MS - (System.currentTimeMillis() - startTime));
+            if ((CHECK_INTERVAL_MS - (System.currentTimeMillis() - startTime)>0))
+                Thread.sleep(CHECK_INTERVAL_MS - (System.currentTimeMillis() - startTime));
             startTime = System.currentTimeMillis();
             System.out.println(ConnectionPool.getDataSource().getNumActive() + "/" + ConnectionPool.getDataSource().getNumIdle());
 
